974135f13b60e5fe46cbcdeeeff5bc3913efccd5
Ifeanyisunday <sundayjnr10@gmail.com>
2024-08-15 12:39:16 +0100

    task app

--- TaskApp/src/main/java/com/TaskApp/service/UserServiceImpl.java
+++ TaskApp/src/main/java/com/TaskApp/service/UserServiceImpl.java
@@ -45,7 +45,7 @@ public class UserServiceImpl implements UserService{
                 return loginResponse;
             }else{
                 LoginResponse loginResponse = new LoginResponse();
-                String name = userOptional.get().getUserName();
+                String name = user.getUserName();
                 loginResponse.setMessage(name + " you are already logged in");
                 return loginResponse;
             }
